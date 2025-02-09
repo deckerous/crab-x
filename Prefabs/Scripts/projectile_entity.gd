dd61163f39ebd12188f46152bf80cccece229bb1
Joshua Chernetsky <joshcherndev@gmail.com>
2025-02-09 04:14:11 -0800

    Cleaned up files.

--- Prefabs/Scripts/projectile_entity.gd
+++ Prefabs/Scripts/projectile_entity.gd
@@ -1,12 +1,22 @@
-extends EntityBase
+class_name ProjectileEntity
+extends Entity
 
-@export var damage: float
-@onready var dir: Vector2
+@export var speed : float = 0.0
 
-@onready var airborne = $CharacterBody2D/StateMachine/Airborne
+@onready var direction : Vector2 = Vector2.ZERO
 
-func _ready() -> void:
-	super()
-	hitbox_component.damage = damage
-	airborne.enter_dir = dir
-	state_machine.change_state("Airborne")
+@onready var destroy = $StateMachine/Destroy
+
+func _physics_process(delta):
+	super(delta)
+	
+	# If the projectile hits a tilemap with collision go to Destroy state
+	var collision_info = move_and_collide(velocity)
+	if collision_info:
+		var collider = collision_info.get_collider()
+		if collider is TileMapLayer:
+			state_machine.change_state(destroy)
+
+func init_projectile(dir: Vector2) -> void:
+	self.direction = dir
+	velocity = dir * speed
