693df3af964e26079bbd5738c8dc10bc174b1b4b
Joshua Chernetsky <joshcherndev@gmail.com>
2025-02-10 01:54:41 -0800

    Updated health bar visuals and made them optionally show.

--- Prefabs/Scripts/entity.gd
+++ Prefabs/Scripts/entity.gd
@@ -1,9 +1,11 @@
 class_name Entity
 extends CharacterBody2D
 
+@export_group("Entity stats")
 @export var max_hp: float = 10
 @onready var hp: float = max_hp
 @export var armor: int = 0
+@export var health_bar_visible: bool = true
 
 @onready var animations = $Animations
 @onready var collision_box = $CollisionBox
@@ -11,6 +13,7 @@ extends CharacterBody2D
 @onready var hurtbox_component = $HurtboxComponent
 @onready var state_machine = $StateMachine
 @onready var health_bar = $HealthBar
+var health_bar_initial_pos = Vector2.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -18,13 +21,17 @@ func _ready() -> void:
 	state_machine.init(self, animations)
 	
 	hurtbox_component.hurt.connect(_damaged)
+	health_bar.visible = health_bar_visible
 	_update_health_bar()
+	health_bar_initial_pos = health_bar.position
 
 func _unhandled_input(event: InputEvent) -> void:
 	state_machine.process_input(event) # state machine input update
 
 func _physics_process(delta: float) -> void:
 	state_machine.process_physics(delta) # state machine physics frame update
+	
+	health_bar.global_position = lerp(health_bar.global_position, global_position + health_bar_initial_pos, 0.25)
 
 func _process(delta):
 	state_machine.process_frame(delta) # state machine physics frame update
@@ -41,6 +48,5 @@ func _damaged(hitbox: HitboxComponent) -> void:
 		queue_free()
 
 func _update_health_bar() -> void:
-	print()
-	print((hp / max_hp))
+	if !health_bar_visible: return
 	health_bar.value = (hp / max_hp) * health_bar.max_value
