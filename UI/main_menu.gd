a2b5cf7a14088c248e4d323a98446b754f95f24e
Joshua Chernetsky <joshcherndev@gmail.com>
2025-02-22 23:28:09 -0800

    Changed background of the main menu.

--- UI/main_menu.gd
+++ UI/main_menu.gd
@@ -1,10 +1,39 @@
-extends CanvasLayer
+extends Control
 
+@onready var crab_container = $CrabContainer
 
+@onready var crab = load("res://Prefabs/Scenes/crab_entity.tscn")
+@onready var crabs = []
 
+@onready var viewport_w = get_viewport().get_visible_rect().size.x
+@onready var viewport_h = get_viewport().get_visible_rect().size.y
 
-func _ready():
-	pass
+func _physics_process(delta):
+	if crabs.size() == 0:
+		_spawn_crabs()
+	
+	if crabs.size() > 0:
+		_update_crabs_pos()
+
+func _spawn_crabs() -> void:
+	var coords = Vector2(randi_range(8, viewport_w - 8), viewport_h)
+	
+	var num = randi_range(1, 8)
+	print(num)
+	for i in num:
+		var inst = crab.instantiate()
+		inst.global_position = Vector2(coords.x + randf_range(-16, 16), coords.y + i * 16)
+		crab_container.add_child(inst)
+		crabs.append(inst)
+
+func _update_crabs_pos() -> void:
+	for crab in crabs:
+		if crab != null:
+			crab.position.y -= 0.5
+			if crab.position.y <= -128:
+				crab.queue_free()
+		else:
+			crabs.remove_at(0)
 
 func _on_new_game_button_pressed():
 	print("New game button pressed")
