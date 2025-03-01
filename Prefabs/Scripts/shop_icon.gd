c11d853692ce2b7ce9fde439bafb423a76f34a7b
Matthew Brosnon <32721082+pwnies2012@users.noreply.github.com>
2025-02-28 19:36:28 -0800

    start work on shop trigger changes

--- Prefabs/Scripts/shop_icon.gd
+++ Prefabs/Scripts/shop_icon.gd
@@ -3,9 +3,6 @@ extends Node2D
 @export var player: Node2D
 
 @onready var collision_box = $EnterCollision/CollisionShape2D
-@onready var timer = $Timer
-@onready var hovered = 0
-@onready var interactable = true
 
 signal enter_shop(sheckles)
 
@@ -15,19 +12,6 @@ func _ready() -> void:
 func _on_area_2d_area_entered(area: Area2D) -> void:
 	# print_debug(area.name)
 	print("Shop_icon.gd, in_shop = ", PlayerVariable.in_shop)
-	if area.name == "CollectableComponent" and interactable:
-		if hovered == 0:
-			enter_shop.emit(player.coin_count)
-		hovered = hovered + 1
-
-func _on_area_2d_area_exited(area: Area2D) -> void:
-	if area.name == "CollectableComponent":
-		hovered = hovered - 1
-
-
-func _on_shop_exit(unused) -> void:
-	interactable = false
-	timer.start(2)
-
-func _on_timer_timeout() -> void:
-	interactable = true
+	print(area.get_parent().is_rally)
+	if area.name == "CollectableComponent" and area.get_parent().is_rally:
+		enter_shop.emit(player.coin_count)
