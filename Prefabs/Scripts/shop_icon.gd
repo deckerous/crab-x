666248f2db5b6104f9fee5e5a56cb182f674fc5b
Matthew Brosnon <pwnies2012@gmail.com>
2025-03-03 19:39:59 -0800

    remove quiver & shop changes

--- Prefabs/Scripts/shop_icon.gd
+++ Prefabs/Scripts/shop_icon.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-@export var player: Node2D
+@export var player: Player
 
 @onready var collision_box = $EnterCollision/CollisionShape2D
 
@@ -13,5 +13,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 	# print_debug(area.name)
 	print("Shop_icon.gd, in_shop = ", PlayerVariable.in_shop)
 	print(area.get_parent().is_rally)
-	if area.name == "CollectableComponent" and area.get_parent().is_rally:
+	if area.get_parent() == player.rally_point_crab_entity:
 		enter_shop.emit(player.coin_count)
